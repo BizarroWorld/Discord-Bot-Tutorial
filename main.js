09a934c013e9952240d6d311c6f0a12acd59162c
skibop <81841025+skibop@users.noreply.github.com>
2021-07-04 20:15:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -36,4 +36,20 @@ client.on("message", (message) => {
   }
 });
 
+readdirSync("./Events/").forEach((file) => {
+  const events = readdirSync("./Events/").filter((file) =>
+    file.endsWith(".js")
+  );
+
+  for (let file of events) {
+    let pull = require(`./Events/${file}`);
+
+    if (pull.name) {
+      client.events.set(pull.name, pull);
+    } else {
+      continue;
+    }
+  }
+});
+
 client.login(process.env.TOKEN);
